be488d8f35fe8c094d0cdf6d0d0eda9067f2f956
bharat <bharatjoshi.p@gmail.com>
2020-08-03 11:22:30 +1000

    add taskbox src

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-docs',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+  ],
+};
---
+++ src/components/Task.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { string, shape, func } from 'prop-types';
+
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
+
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name='checked'
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)}/>
+      </label>
+      <div className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          placeholder="Input title"
+          style={{ textOverflow: 'ellipsis' }}
+        />
+      </div>
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        { state !== 'TASK_ARCHIVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-start`} />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+  // return (
+  //   <div className="list-item">
+  //     <input type="text" value={title} readOnly={true}/>
+  //   </div>
+  // )
+};
+
+Task.propTypes = {
+  task: shape({
+    id: string.isRequired,
+    title: string.isRequired,
+    state: string.isRequired,
+  }),
+  onArchiveTask: func,
+  onPinTask: func,
+};
+
+export default Task;
